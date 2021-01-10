1bdbcca054d9d4e23553b5dd7451b2e290fa20f6
Toranova <chia_jason96@live.com>
2021-01-10 21:00:53 +0800

    updated versioning

--- viauth/vial.py
+++ viauth/vial.py
@@ -4,5 +4,5 @@ this file contains the implementation description/version that should be changed
 upon every build
 '''
 name = 'viauth'
-version = '0.1.1'
+version = '0.1.3'
 description = 'vial-auth (viauth), a flask mini login/auth module'
