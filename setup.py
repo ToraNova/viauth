ec7d2fd5683d3ea4ae60dbe54caecf931308242a
Toranova <chia_jason96@live.com>
2021-04-10 19:51:43 +0800

    vicore version update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     keywords = ['Flask', 'Authentication'],
     install_requires=[
         'flask',
-        'vicore>=0.1.1',
+        'vicore',
         'flask-login',
         'sqlalchemy',
     ],
